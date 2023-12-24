d7a4e1ac08b6b3aa5263bf4b22d998e6484bd8c5
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-24 18:46:46 +0900

    Remove unused import statement

--- BatchSampleChunk/src/main/java/com/example/demo/BatchSampleChunkApplication.java
+++ BatchSampleChunk/src/main/java/com/example/demo/BatchSampleChunkApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing // Job,Step,Bean定義などを自動で設定してくれるアノテーション
 public class BatchSampleChunkApplication {
 
 	public static void main(String[] args) {
