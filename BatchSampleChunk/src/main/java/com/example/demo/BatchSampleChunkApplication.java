3490585f8472afb5c4d0bbdc3d9691e121feed0b
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-24 17:53:36 +0900

    Add EnableBatchProcessing annotation to BatchSampleChunkApplication.java

--- BatchSampleChunk/src/main/java/com/example/demo/BatchSampleChunkApplication.java
+++ BatchSampleChunk/src/main/java/com/example/demo/BatchSampleChunkApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // Job,Step,Bean定義などを自動で設定してくれるアノテーション
 public class BatchSampleChunkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchSampleChunkApplication.class, args);
 	}
-
 }
