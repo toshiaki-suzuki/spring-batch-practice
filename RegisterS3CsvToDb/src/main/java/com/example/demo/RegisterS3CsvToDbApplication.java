cbda72bb635623b26baba212eded2b0d47900227
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-25 08:09:18 +0900

    Add initial project files

---
+++ RegisterS3CsvToDb/src/main/java/com/example/demo/RegisterS3CsvToDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterS3CsvToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterS3CsvToDbApplication.class, args);
+	}
+
+}
